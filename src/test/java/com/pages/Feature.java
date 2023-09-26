3fb3aa8c5f771b8e8e5f6550b0fb6416f578a058
Ramesh <rameshbabugvs@gmail.com>
2023-09-26 07:14:17 +0530

    feature java file added

---
+++ src/test/java/com/pages/Feature.java
@@ -0,0 +1,12 @@
+package com.pages;
+
+public class Feature {
+public static void f1() {
+	System.out.println("f1 feature");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+f1();
+	}
+
+}
