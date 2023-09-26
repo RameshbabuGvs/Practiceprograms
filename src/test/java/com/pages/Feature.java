0250aa534326e371c8e5233407685e74cf530809
Ramesh <rameshbabugvs@gmail.com>
2023-09-26 07:45:41 +0530

    feature 2

--- src/test/java/com/pages/Feature.java
+++ src/test/java/com/pages/Feature.java
@@ -4,6 +4,10 @@ public class Feature {
 public static void f1() {
 	System.out.println("f1 feature");
 }
+
+public static void f2() {
+	System.out.println("f2 feature");
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 f1();
